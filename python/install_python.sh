4ce5a3c64ae4ad4bf147fc91df000877c9241e03
Idea Bosque <ideabosque@gmail.com>
2024-07-30 14:38:53 -0700

    Use rockylinux.

--- python/install_python.sh
+++ python/install_python.sh
@@ -3,11 +3,7 @@ cd /tmp
 wget https://www.python.org/ftp/python/3.10.9/Python-3.10.9.tgz
 tar xzf Python-3.10.9.tgz
 cd Python-3.10.9/
-
-export CFLAGS="$CFLAGS $(pkg-config --cflags openssl11)"
-export LDFLAGS="$LDFLAGS $(pkg-config --libs openssl11)"
 ./configure --enable-optimizations
-
 make altinstall
 
 # Clear up
